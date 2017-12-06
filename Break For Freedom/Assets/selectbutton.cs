523215bba44cb948c947ee88de7b0d8af41e88a5
KBC11606068\OS <naoyuki-i-16d@stu.kbc.ac.jp>
2017-12-06 11:31:01 +0900

    [fix]タイトル画面編集

---
+++ Break For Freedom/Assets/selectbutton.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class selectbutton : MonoBehaviour {
+    Button start;　//スタートボタン
+    Button end;   //エンドボタン
+ 
+    public bool LoadGames;//セーブデータの有無
+
+	// Use this for initialization
+    void Start()
+    {
+        //StartUIと言うキャンバスのSTARTという名前のボタンオブジェクトを取得
+        start = GameObject.Find("Canvas/startbutton").GetComponent<Button>();
+
+        //StartUIと言うキャンバスのLoadという名前のボタンオブジェクトを取得
+        end = GameObject.Find("Canvas/endbutton").GetComponent<Button>();
+
+        if (!LoadGames)
+        {　　//セーブデータがなかったら
+
+            start.Select();
+
+        }else{　　　　　　//それ以外(セーブデータがあるとき)
+
+            end.Select();
+
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+    }
+}
